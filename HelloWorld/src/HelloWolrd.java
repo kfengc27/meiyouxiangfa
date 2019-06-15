083eb30360d2959892bb4e25112eab468c413bfb
Chengbin <kfengc27@gmail.com>
2019-06-15 23:44:28 +1000

    add all files 14 june 2019

---
+++ HelloWorld/src/HelloWolrd.java
@@ -0,0 +1,25 @@
+
+public class HelloWolrd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String person_name_gw = "Tody";
+		String person_name_xy = "Yg";
+		
+		int age_of_tody = 12;
+		int age_of_xy = 20;
+		
+		double wechat_money_of_tody = 450.76;
+		float wechat_money_of_kevin = (float) 7.6; 
+		
+		System.out.println("Xiaoyong's English name is " + person_name_xy);
+		System.out.println("Feng Guangwei's age is " + age_of_tody);
+		
+		int age_of_xiaoyong_and_fgw = age_of_tody + age_of_xy; 
+		
+		System.out.println("The total age of xiaoyonga and fgw is " + age_of_xiaoyong_and_fgw );
+
+	}
+
+}
